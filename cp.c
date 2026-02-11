9f8c2837ccf34011b0310def7d9eaf35f8f6dd65
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-02-11 19:55:36 +0530

    Args, Source file and Destination file validation

--- cp.c
+++ cp.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
@@ -9,12 +11,13 @@ int main(int argc, char *argv[])
   {
     printf("Error 01: Invalid Number of arguements!\n");
     printf("Please enter the source and final destinations\n");
-    return 0;
+    return 1;
   }
   else if (argc > 3)
   {
     printf("Error 01: Invalid Number of arguements!\n");
     printf("Please enter only a source and a final destination!\n");
+    return 1;
   }
   // else
   // {
@@ -28,6 +31,33 @@ int main(int argc, char *argv[])
   // ik i need a char buffer for it
   // size of the buffer i am not sure
   // i also need to check if the sources exists or not even
+  // lets first go and check validity of the files
+
+  // lets check if they even exist or not
+
+  int chk_src = open(argv[1], O_RDONLY);
+  if (chk_src < 0)
+  {
+    printf("Error 02: Source file not found.\n");
+    return 1;
+  }
+  // printf("Source file found.\n")
+
+  int chk_dest = open(argv[2], O_RDONLY);
+  if (chk_dest < 0)
+  {
+    printf("Error 02: Destination not found.\n");
+    return 1;
+  }
+  // printf("Destination file found.\n")
+
+  // validation done for src and dest when botha re files
+  // i want to make it work for fianl destination as folderas well
+  // i willl hv to create a new file inside the dir using the same name and type
+  // so i hv to scrap the name from source as well
+
+  // next: make the copy loop using the buffer to copythe file to a file
+  // i will implement the file creation inside a folder later
 
   return 0;
 }
